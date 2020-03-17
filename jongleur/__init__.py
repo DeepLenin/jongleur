4ff0b141f184d7841765e3463e1f2bc8cedeeeb6
Alex Gaziev <alex.gaziev@gmail.com>
2020-03-17 06:48:38 -0700

    Init commit

---
+++ jongleur/__init__.py
@@ -0,0 +1,9 @@
+"""
+The main point of this package is to give easy and standardized way to infer on
+several sets of data and analyze differences of outputs.
+
+We plan to give convention to use this instrument properly with any framework
+and kind of data you possibly need: basically you'll implement most of low
+level coding which will be really similar to code you use in your
+training/validation pipeline.
+"""
